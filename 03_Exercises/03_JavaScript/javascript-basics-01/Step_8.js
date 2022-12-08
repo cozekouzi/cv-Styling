4271db2b888bf541e9b1a43ea312feb3edad5330
cozekouzi <omarkouzi81248@gmail.com>
2022-12-08 10:54:21 +0000

    Step-8

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_8.js
@@ -0,0 +1,8 @@
+var validate = document.getElementById("validate").onclick = function(){
+    var age = document.getElementById("age").value;
+    if(age>=18)
+    alert("you are over 18");
+    else
+    alert("you are under 18");
+}
+
