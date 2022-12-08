90d2bbf6352a93854e8d08ba11395409c4bb8460
cozekouzi <omarkouzi81248@gmail.com>
2022-12-08 18:52:14 +0000

    fixing step-5

--- 03_Exercises/03_JavaScript/javascript-basics-02/step 5/Step-5.js
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 5/Step-5.js
@@ -1,25 +1,4 @@
-
-
-var image1 = document.getElementById("image1");
-var image2 = document.getElementById("image2");
-var image3 = document.getElementById("image3");
-var image4 = document.getElementById("image4");
-var image5 = document.getElementById("image5");
-
-image1.addEventListener('mouseover', function(){
-  image1.src = "images/image1_2.jpg"
-  image2.src = "images/image2_2.jpg"
-  image3.src = "images/image3_2.jpg"
-  image4.src = "images/image4_2.jpg"
-  image5.src = "images/image5_2.jpg"
-
-
-})
-
-
-
-
-
-
-
-
+document.querySelectorAll('img').forEach((img,i)=>{
+  img.addEventListener('mouseover', ()=>{
+    img.src = `images/image${i + 1}_2.jpg`;})
+});
