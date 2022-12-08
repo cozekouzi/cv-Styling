a7eb15a1ed6ee5342bc342621caea655da02f5a2
cozekouzi <omarkouzi81248@gmail.com>
2022-12-08 10:18:58 +0000

    Step-5

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_5.js
@@ -0,0 +1,8 @@
+let validate = document.getElementById("validate").onclick = function() {
+    let first_number = document.getElementById("first_number").value;
+    let second_number = document.getElementById("second_number").value;
+
+alert("result="+first_number*second_number);
+
+
+}
