3bc136618cc2771926c6e8867367dfb35cf64bb0
cozekouzi <omarkouzi81248@gmail.com>
2022-12-08 11:39:42 +0000

    Step-1

---
+++ 03_Exercises/03_JavaScript/javascript-basics-02/step 1/Step-1.js
@@ -0,0 +1,17 @@
+
+
+var image = document.getElementById("image1");
+
+
+image.addEventListener('mouseover', function(){
+  image.src = "images/image1_2.jpg"})
+image.addEventListener('mouseout', function(){
+    image.src = "images/image1.jpg"
+  })
+
+
+
+
+
+
+
