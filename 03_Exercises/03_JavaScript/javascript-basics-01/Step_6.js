50aa795e519f388bfec199d7adfd1f530b26e6f3
cozekouzi <omarkouzi81248@gmail.com>
2022-12-08 10:38:51 +0000

    Step-6

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_6.js
@@ -0,0 +1,6 @@
+var validate = document.getElementById("validate").onclick = function(){
+  var firstnb = document.getElementById("first_number").value;
+  var secondnb = document.getElementById("second_number").value;
+   
+    alert("remainder= "+firstnb%secondnb);
+}
