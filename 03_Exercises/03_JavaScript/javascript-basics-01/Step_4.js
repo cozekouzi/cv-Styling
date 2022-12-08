4063b85e8895f6902761c9431e5f494ce0e136fd
cozekouzi <omarkouzi81248@gmail.com>
2022-12-08 10:01:14 +0000

    Step-4

---
+++ 03_Exercises/03_JavaScript/javascript-basics-01/Step_4.js
@@ -0,0 +1,10 @@
+document.getElementById("validate").onclick = function(){
+     let name = document.getElementById("name").value;
+     let surname = document.getElementById("surname").value;
+     let city = document.getElementById("city").value;
+     alert ("Name: "+name+"\nSurname: "+surname+"\nCity: "+city);
+
+    
+}
+
+
