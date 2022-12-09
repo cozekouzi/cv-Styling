351ca7f5b84c5cc0daac05bdb0b1a60e13b791f6
cozekouzi <omarkouzi81248@gmail.com>
2022-12-09 08:41:26 +0000

    step-2

---
+++ 03_Exercises/03_JavaScript/javascript-basics-03/step 2/Step-2.js
@@ -0,0 +1,21 @@
+
+function show_hide(x){
+
+if(x==1)
+    {document.getElementById("texte").style.display="inline";
+    }
+else 
+   { document.getElementById("texte").style.display="none";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
