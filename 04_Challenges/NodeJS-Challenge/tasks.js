25813d0dbdc4978ca76ea259ed873b99c719cee3
cozekouzi <omarkouzi81248@gmail.com>
2023-01-02 13:19:56 +0000

    added done property to tasks

--- 04_Challenges/NodeJS-Challenge/tasks.js
+++ 04_Challenges/NodeJS-Challenge/tasks.js
@@ -1,3 +1,5 @@
+//*global variables
+const mylist = [];//! dummy list delete content when finnish
 
 /**
  * Starts the application
@@ -15,7 +17,9 @@ function startApp(name){
   process.stdin.on('data', onDataReceived);
   console.log(`Welcome to ${name}'s application!`);
   console.log("--------------------");
-  help();//so the user knows what this app do
+  //*------- so the user knows what this app do
+  help();
+  pushdummytomylist();
 }
 
 
@@ -35,16 +39,16 @@ function startApp(name){
  * @returns {void}
  */
 function onDataReceived(text) {
-  //turn input to lowercase letters so that no error ocure if upper case letters appear
+  //*------- turn input to lowercase letters so that no error ocure if upper case letters appear
  
-  // get commands and params
+  //*------- get commands and params
   text=text.replace('\n','');
   var i = text.indexOf(' ');
   if (i==-1) i=text.length;
   var mycommand = text.substring(0,i).toLowerCase();
   var myparams = text.substring(i+1);
 
-  //dispature
+  //*------- dispature
   if (mycommand == 'quit') quit();
   else if(mycommand == 'exit') exit();
   else if(mycommand == 'hello') hello(myparams);
@@ -105,10 +109,11 @@ function quit(){
   console.log('-----------------------');
   process.exit();
 }
-//help functions outputs all commands available for the user and the role of each one
+//*======= help functions outputs all commands available for the user and the role of each one
 function help(){
   console.log('----')
   console.log('add:"Adds a task to My list."');
+  console.log('edit:"Edits tasks"')
   console.log('exit:"Exits the application."');
   console.log('hello:"Says hello plus the extra text the user inputs."');
   console.log('help:"Outputs all commands available for the user and the role of each one."');
@@ -117,15 +122,14 @@ function help(){
   console.log('remove:"Removes a certain task from My list."')
   console.log('----------------------------------');
 }
-// The following line starts the application
+//* The following line starts the application
 startApp('Omar kouzi');
-//help function
 
-//======= hello function
+//*======= hello function
 function hello(name1){
-  //------- remove empty spaces before and after the content then add an initial space as a separaitor from "hello"
+  //*------- remove empty spaces before and after the content then add an initial space as a separaitor from "hello"
   name1 =' '+ name1.trim();
-  //------- in case of an empty content remove the empty space added above
+  //*------- in case of an empty content remove the empty space added above
   if(name1 == ' '){
     name1='';
   }
@@ -145,67 +149,109 @@ function hello(name1){
 //*****************************************************************
 
 
-// list function
-const mylist = ["aks","ins","osa","asoa"];
+//*======= list function
+function pushdummytomylist(){
+  add("abed",false);
+  add("omar",true);
+  add("mgo",false);
+  add("hicup",false);
+}
+
+
+
 function list(){
 console.log('My List\n'+'--------');
 mylist.forEach((task, i)=>{
-  console.log(`${i+1}`+' - '+task);
+log_atasklist(task,i)
 });
 }
-// add function
-function add(newtask){
+//*======= add function
+function add(newtask,status){
   newtask = newtask.trim();
   if (newtask == '')
   {
     console.log('no task to add.')
   } 
  else{
-  mylist.push(newtask);
+  add_atasklist(newtask,status);
   console.log(`${newtask} was added to your list`);
  }
   console.log('----------------------------');
 
 }
-// remove function
+//*======= remove function
 function remove(i){
   if(i > mylist.length){
     console.log("Can't remove task :'task not found'")
   }
   else{
- //remove element at index at i-1 hence it's zero based
- mylist.splice(i-1,1);
- //called for businuss presentation
+ //*------- remove element at index at i-1 hence it's zero based
+ remove_atasklist(i-1);
+ //*------- called for businuss presentation
  list();
   }
  
 }
 
-//edit function
+//*======= edit function
 function edit(text) {
+  //*------- error if there's nothing to edit
   if(text == ''){
     console.log('error!!');
     return;
   }
   var i = text.indexOf(' ');
+  //*------- if the task isn't defined the last task will be edited 
   if (i==-1) {
     var task_numb = mylist.length;
     var edited_task = text ;
-    mylist[task_numb-1] = edited_task;
+    edit_atasklist(task_numb-1,edited_task,false);
    }
+   //*------- editing specefice task
   else{
     var task_numb = text.substring(0,i).toLowerCase();
     var edited_task = text.substring(i+1); 
-    mylist[task_numb-1] = edited_task;
+    edit_atasklist(task_numb-1,edited_task,false);
  
   }
-   
- 
-  
 }
 
 
+//******************************************************************
+//*****************************************************************
+//******* task list manegment
+//*****************************************************************
 
+
+function add_atasklist(task,status){
+ 
+  if (status == true){
+    task = "[✓] " + task;
+  }
+  else{
+    task = "[ ] " + task;
+  }
+  mylist.push(task);
+}
+function edit_atasklist(i,task,status){
+  if (status == true){
+    task = "[✓] " + task;
+  }
+  else{
+    task = "[ ] " + task;
+  }
+  mylist[i] = task;
+}
+function remove_atasklist(i){
+  mylist.splice(i,1);
+}
+function log_atasklist(task,i){
+  console.log(`${i+1}`+' - '+task);
+
+}
+function isdone(){
+
+}
 
 
 
