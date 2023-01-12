e82535d19b610da0a7bb0fcb8e72b8095d9b62f0
cozekouzi <omarkouzi81248@gmail.com>
2023-01-12 13:05:57 +0000

    step

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express')
 const express = require('express')
 const app = express()
 const port = 4000
@@ -5,10 +6,10 @@ const date = new Date()
 const hour = date.getHours()
 const minuts = date.getMinutes()
 
-
-app.get('/', function (req, res){
-  res.send('hello world!!')
-})
+app.get('/hello/:id?', function(req, res){
+  const idd = req.params.id;
+  res.send({status:200, message:`hello, ${idd}`});
+});
 app.get('/test', function (req, res){
   res.send({status:200, message:"ok"})
 })
@@ -20,11 +21,17 @@ app.get('/time', function (req, res){
     res.send({status:200, message:`${hour + ":0"+minuts}`})
   }
 })
-
+app.get('/search',(req,res) => {
+  const s=req.query.s
+  if (s) {
+    res.send({status:200, message:"ok", data:s})
+  }
+  else{
+    res.send({status:500, error:true, message:"you have to provide a search"})
+  }
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-
-  
+  console.log(`Example app listening on port ${port}`)  
 })
 
 
